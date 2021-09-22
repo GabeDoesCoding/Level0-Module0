01eee9e79dbdcb2b450986172a20db79573a6337
GabeDoesCoding <gabelorensd@gmail.com>
2021-09-21 18:30:59 -0700

    Finished up to flaming ninja star

--- _02_code_flow/_a_turtle_spiral.py
+++ _02_code_flow/_a_turtle_spiral.py
@@ -14,15 +14,19 @@ if __name__ == '__main__':
     window.bgcolor('white')
     
     # Make a new turtle
-    joe=turtle.Turtle
+    joe = turtle.Turtle()
     # This code sets our shape to a turtle
-    joe.shape(turtle)
+    joe.shape('turtle')
     # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
-    
+    joe.speed=10
     # Set your turtle's color using .color('green')
-    
+    joe.color('green')
     # Use a loop to repeat a the code below 50 times
-        
+    for i in range(50) :
+         joe.color(get_random_color())
+         joe.forward(5*i)
+         joe.right(360/7)
+         joe.width(i)
         # Set the turtle color to a random color
 
         # Move the turtle (5*i) pixels. 'i' is the loop variable
