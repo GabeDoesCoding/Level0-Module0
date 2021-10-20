942cc6d785366320a9f43cb1c188f522e15f18d5
GabeDoesCoding <gabelorensd@gmail.com>
2021-10-19 18:28:41 -0700

    Finished up to Terminator Cat

--- _03_print_and_popups/_d_quiz_game.py
+++ _03_print_and_popups/_d_quiz_game.py
@@ -9,16 +9,30 @@ if __name__ == '__main__':
     # 1. Create a variable to hold the user's score. Set it equal to zero. 
     score = 0
     # ASK A QUESTION AND CHECK THE ANSWER
-    #      // 2.  Ask the user a question 
-    Q1 = simpledialog.askstring(title='Quiz Game', prompt="What is 2x(-3)x Y = 36, what does Y equal?")
+    messagebox.showinfo(title='Quiz Game', message="Welcome to the quiz game! You will be asked a series of questions which will add points to your score if answered correctly. However, if you answer incorrectly, points will be taken from your score. Good luck!")
+    #      // 2.  Ask the user a question
+    Q1 = simpledialog.askinteger(title='Quiz Game', prompt="What is 2x(-3)x Y = 36, what does Y equal?")
     #      // 3.  Use an if statement to check if their answer is correct
-    if Q1 == -6:
+    if Q1 ==-6:
     #      // 4.  if the user's answer was correct, add one to their score 
         score +=1
     # MAKE MORE QUESTIONS. Ask more questions by repeating the above 
     #      // Option: Subtract a point from their score for a wrong answer
- 
+    else:
+        score+=-1
+    Q2 = simpledialog.askinteger(title='Quiz Game', prompt="What is (-64 ÷ 4) + (-5)?")
+    if Q2==-21:
+        score +=1
+    else:
+        score +=-1
+    Q3 = simpledialog.askinteger(title='Quiz Game', prompt="What is 5+(-7)+15+(-13)?")
+    if Q3 ==0:
+        score+=1
+    else:
+        score+=-1
     # After all the questions have been asked, tell the user their final score
     # remember to convert your variable to a string using the str() function 
-    
+    messagebox.showinfo(title='You finished!', message="Congratulations on completing the quiz! Your final score is...")
+    messagebox.showinfo(title='You finished!', message= str(score) + "!")
     # Run the window's .mainloop() method
+    window.mainloop()
--- _03_print_and_popups/_e_awesome_or_not.py
+++ _03_print_and_popups/_e_awesome_or_not.py
@@ -2,27 +2,33 @@ from tkinter import messagebox, simpledialog, Tk
 import random
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
-    # Make a new window variable, window = Tk()
-    
-    # Hide the window using the window's .withdraw() method
+if __name__ == '__main__':
 
+    # Make a new window variable, window = Tk()
+    window = Tk()
+    # Hide the window using the window's .withdraw() method
+    window.withdraw()
     # 1. Make a variable equal to a positive number less than 4 using random.randInt(0, 3)
-    
+    ran = random.randint(0,3)
     # 2. Print your variable to the console
-    
+    print()
     # 3. Get the user to enter something that they think is awesome
-    
+    simpledialog.askstring(title='Awesome', prompt="What is something you think is awesome?")
     # 4. If your variable is  0
         # -- tell the user whatever they entered is awesome!
-        
+    if ran == 0:
+            messagebox.showinfo(title='Awesome!', message="What you entered is awesome!")
     # 5. If your variable is  1
         # -- tell the user whatever they entered is ok.
-    
+    if ran == 1:
+            messagebox.showinfo(title='Okay', message="What you entered is okay.")
     # 6. If your variable is  2
         # -- tell the user whatever they entered is boring.
-    
+    if ran == 2:
+            messagebox.showinfo(title='Boring', message="What you entered is boring...")
     # 7. If your variable is  3
         # -- invent your own message to give to the user (be nice).
-        
+    if ran == 3:
+            messagebox.showinfo(title='Incredible!' , message= "What you entered is really incredible!")
     # Run the window's .mainloop() method
+    window.mainloop()
