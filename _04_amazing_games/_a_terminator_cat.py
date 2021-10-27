7b15fc592271430e44e3018c645ebeaeecff6939
GabeDoesCoding <gabelorensd@gmail.com>
2021-10-26 18:31:40 -0700

    Finished up to Terminator Cat

--- _04_amazing_games/_a_terminator_cat.py
+++ _04_amazing_games/_a_terminator_cat.py
@@ -61,16 +61,16 @@ if __name__ == '__main__':
     
     # 2. Call the set_background() function with your variable inside of the parenthesis
     #    for example, set_background(bg_image)
-
+    set_background("bigEyedCat2.gif")
     # 3. Make a new turtle
-
+    joe = turtle.Turtle()
     # 4. Set the turtle color and pen color to red (or any color you want)
     #    using .color('red', 'red')
-
+    joe.color('red')
     # 5. Set the turtle width to 0 so no outlines are drawn
-
+    joe.width(0)
     # 6. Set the turtle speed to 0 (fastest)
-
+    joe.speed(0)
     # 7. Run the program and click on one of the cat's eyes. 
     #    The x,y position of the eye will be printed at the bottom of your
     #    processing window.
@@ -82,10 +82,11 @@ if __name__ == '__main__':
     #    and initialize them:
     #    left_eye  = Eye(eye=my_turtle, x=-34, y=11, radius=30)
     #    right_eye = Eye(eye=my_turtle, x=40, y=-5, radius=30)
-
+    left_eye = Eye(eye=joe, x=47, y=68)
+    right_eye = Eye(eye=joe, x=1, y=45)
     # 9. Call the .draw() method on BOTH eye variables
-
-
+    left_eye.draw()
+    right_eye.draw()
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
     window.onkeypress(key_pressed, 'space')
--- _04_amazing_games/_b_high_low_game.py
+++ _04_amazing_games/_b_high_low_game.py
@@ -12,12 +12,19 @@ if __name__ == '__main__':
     # 2. Print out the random variable that you made in step #1
     print(ran)
     # 3. Code a for loop to run steps 4-10, 10 times
-
+    for i in range(10):
         # 4. Ask the user for a guess using a pop-up window, and save their response
-
+        resp=simpledialog.askinteger(title='Guess the Number!', prompt="Guess a number between 1 and 100!")
         # 5. If the guess is correct
+        if resp== ran:
+            messagebox.showinfo(title='You did it!', message='Congratulations! You guessed the number!')
+            sys.exit(0)
+        if resp > ran:
+            messagebox.showinfo(title='Too High', message='Oops! A bit too high. Try again!')
+        else:
+            messagebox.showinfo(title='Too low!', message='That was a bit too low! Try again!')
             # 6. Win. Use 'sys.exit(0)' to end the program
-
+    messagebox.showinfo(title='You were so close!', message='You were so close to getting the number! It was ' + str(ran) +'! You will get it next time!')
         # 7. if the guess is high
             # 8. Tell them it's too high
         # 9. Else if the guess is low
